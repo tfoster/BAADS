3b8c2be8859c4f384a0a5140e16e19536c773efc
Tyler Foster <tyler.foster@evolv.ai>
2022-07-04 13:04:57 -0700

    Scoping sublocations to unclutter map

--- screens/NavigationScreen.tsx
+++ screens/NavigationScreen.tsx
@@ -19,25 +19,14 @@ import {RootTabScreenProps} from '../types';
 import locations from '../locations.json';
 import strings from '../strings.json';
 
-function flattenLocations(key, location) {
-  const cloned = Object.assign({}, location);
-  delete cloned.sublocations;
-  cloned.key = key;
-
-  let results = [cloned];
-  if (location.sublocations) {
-    results = results
-        .concat(Object.entries(location.sublocations).reduce((a, [k, l]) => a.concat(flattenLocations(k, l)), []));
-  }
-  return results;
-}
-
-const flattenedLocations = Object.entries(locations).reduce((a, [k, l]) => a.concat(flattenLocations(k, l)), []);
-
+const DEFAULT_MAP_CONTEXT = 'south_beach_harbor';
 export default function NavigationScreen({ navigation }: RootTabScreenProps<'Navigation'>) {
   const [errorMsg, setErrorMsg] = useState('');
   const [location, setLocation] = useState<LocationObject>(null);
-  const [mapContext, setMapContext] = useState();
+  const [mapContext, setMapContext] = useState([DEFAULT_MAP_CONTEXT, locations[DEFAULT_MAP_CONTEXT]]);
+
+  const mapContextKey = mapContext[0];
+  const sublocations = mapContext[1]['_sublocations'];
   const [region, setRegion] = useState({
     latitude: 37.78015223385197,
     longitude:  -122.38790713099024,
@@ -72,11 +61,12 @@ export default function NavigationScreen({ navigation }: RootTabScreenProps<'Nav
         initialRegion={region}
         onRegionChangeComplete={region => setRegion(region)}
     >
-      {flattenedLocations
-          .filter(l => l.contexts?.contains(mapContext) || !mapContext && !l.contexts)
-          .map((l => (<Marker key={l.key} coordinate={l} title={strings.locations[l.key].name}>
+      { sublocations && Object.entries(sublocations)
+          .filter(([k, l]) => !k.startsWith('_'))
+          .map(([k, l]) => ((<Marker key={k} coordinate={l} title={strings.locations[k].name}>
             <Icon name={l.icon || 'close'} type={l.icon_family || 'material-community'}/>
-          </Marker>)))}
+          </Marker>)))
+      }
       { location ? <Marker coordinate={location?.coords ?? region} /> : undefined }
     </MapView>
   );
